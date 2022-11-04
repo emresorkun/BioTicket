05a5c64ce62355d9c9ab308e9a7639256e3a6544
Emre Sorkun <emresorkun@gmail.com>
2022-11-04 14:25:11 +0100

    menu and loop created and constructor added

--- BioTicket/Customer.cs
+++ BioTicket/Customer.cs
@@ -8,28 +8,51 @@ namespace BioTicket
 {
     internal class Customer
     {
-        public string firstName;
-        public string lastName;
-        public int birthYear;
+
+        //fields should not be public! they are private! Properties shoud be public! not 
+        private string lastName;
+        private string firstName;
+        private int birthYear;
+
+        public int BirthYear { get; set; }
+
+        public string FirstName { get; set; }
 
         public Customer(string first, string last, int birth)
         {
+            //firstName = first;
             firstName = first;
             lastName = last;    
-            birthYear = birth;  
+            BirthYear = birth;
         }
-    public void AgeCalculater(int birthYear)
+
+        //this can go to another class!!!
+    public void AgeCalculater()
         {
-            int age=2023-birthYear;
+            int age=2022-BirthYear;
             Console.WriteLine($"{firstName} {lastName}, is {age} years old.");
             
         }
-    public  void AgeController (int age)
+
+        public void AgeCalculater2(Customer customer, int test = 2)
+        {
+            
+            int age = 2022 - customer.BirthYear;
+            Console.WriteLine($"{customer.firstName} {customer.lastName}, is {age} years old.");
+
+        }
+        //this can go to another class!!!
+        public void AgeController (int age)
         {
             if (age > 20)
             {
                 Console.WriteLine("youth discount is not possible");
             }
+        }
+
+        public void DisplayCustomerDetails()
+        {
+            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nBirthyear: \t{birthYear}"); 
         }
     }
 }
--- BioTicket/Program.cs
+++ BioTicket/Program.cs
@@ -2,8 +2,44 @@
 
 using BioTicket;
 
-Console.WriteLine("create a customer");
-Console.WriteLine("-----------------\n");
+bool myBool = false;
+do
+{
+    Console.WriteLine("Welcome to the main menu");
+    Console.WriteLine("In order to test different functions");
+    Console.WriteLine("0 will end this programme");
+    Console.WriteLine("1 will suprise you");
+    Console.WriteLine("-----------------\n");
+    string userInput = Console.ReadLine()!;
+    switch (userInput)
+    {
+        case "0":
+            {
+                Console.WriteLine("the programme is shutting down");
+                myBool = true;
+            }
+            break;
+        case "1":
+            Console.WriteLine("WRONG INPUT");
+            break;
+        case "2":
 
-Customer newCustomer = new Customer("emre", "sorkun", 1979);
-Console.WriteLine(newCustomer.birthYear);
+            break;
+        default:
+            break;
+    }
+
+
+    //this never ending loop 3 menu option? 
+    //never ending loop
+} while (!myBool);
+
+//Customer cust2 = new Customer("Test", "Testsson", 1980);
+//Customer emre = new Customer("emre", "sorkun", 1979);
+//Console.WriteLine(emre);
+//emre.DisplayCustomerDetails();
+
+//emre.AgeCalculater();
+
+
+//emre.AgeCalculater2(cust2, 5);
